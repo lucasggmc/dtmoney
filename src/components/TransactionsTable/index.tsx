d8e5cde54241b4116c902fce8e8f4678658ba127
lucasggmc <lucasggmc@gmail.com>
2021-07-19 23:29:29 -0300

    adicionado modal de transações

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+import { Container } from './styles';
+
+export function TransactionsTable(){
+
+    useEffect(() => {
+        api.get('transactions')        
+        .then(response => console.log(response.data))
+    }, [])
+
+
+    return (
+       <Container>
+           <table>
+               <thead>
+                   <tr>
+                       <th>Título</th>
+                       <th>Valor</th>
+                       <th>Categoria</th>
+                       <th>Data</th>
+                   </tr>
+               </thead>
+               <tbody>
+                   <tr>
+                        <td>Teste</td>
+                        <td className="deposit">Teste</td>
+                        <td>Teste</td>
+                        <td>Teste</td>
+                   </tr>
+
+                   <tr>
+                        <td>Teste</td>
+                        <td className="withdraw">-Teste</td>
+                        <td>Teste</td>
+                        <td>Teste</td>
+                   </tr>
+
+                   <tr>
+                        <td>Teste</td>
+                        <td>Teste</td>
+                        <td>Teste</td>
+                        <td>Teste</td>
+                   </tr>
+
+               </tbody>
+           </table>
+       </Container> 
+    );
+}
